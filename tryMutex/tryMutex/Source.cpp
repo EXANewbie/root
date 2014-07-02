f051834e3b2ac7c4da9d0fa9880ac2ab897708b1
Jeong-hwan Yim <gg7308@bluehole.net>
2014-07-02 15:59:01 +0900

    Producer using class

--- tryMutex/tryMutex/Source.cpp
+++ tryMutex/tryMutex/Source.cpp
@@ -9,63 +9,48 @@
 
 using namespace std;
 
-//class SYNCHED_QUEUE
-//{
-//private :
-//	queue<int> que;
-//	mutex mtx;
-//
-//	mutex push_enabled;
-//
-//	const int MAX_COUNT = 10;
-//public :
-//	SYNCHED_QUEUE() {}
-//	void enqueue(int element)
-//	{
-//		mtx.lock();
-//		que.push(element);
-//		mtx.unlock();
-//		//
-//	}
-//	int dequeue()
-//	{
-//		//
-//	}
-//	void push_waited() {
-//		mtx.lock();
-//		push_enabled.lock();
-//		mtx.unlock();
-//	}
-//
-//	bool is_full() {
-//		return que.size() < MAX_COUNT;
-//	}
-//};
-queue<int> que;
-mutex mtx;
-
-mutex push_enabled;
-
-const int MAX_COUNT = 10;
-void enqueue(int element)
+class SYNCHED_QUEUE
 {
-	mtx.lock();
-	que.push(element);
-	mtx.unlock();
-	//
-}
-bool is_full() {
-	return que.size() < MAX_COUNT;
-}
-void Producer_run(/*SYNCHED_QUEUE *queue, */int index) {
+private :
+	queue<int> que;
+	mutex mtx;
+
+//	mutex push_enabled;
+
+	const int MAX_COUNT = 10;
+public :
+	SYNCHED_QUEUE() {}
+	void enqueue(int element)
+	{
+		mtx.lock();
+		que.push(element);
+		mtx.unlock();
+		//
+	}
+	int dequeue()
+	{
+		//
+	}
+	//void push_waited() {
+	//	mtx.lock();
+	//	push_enabled.lock();
+	//	mtx.unlock();
+	//}
+
+	bool is_full() {
+		return que.size() < MAX_COUNT;
+	}
+};
+
+void Producer_run(SYNCHED_QUEUE* queue, int index) {
 	srand(time(NULL));
 
 	while (true) {
 //		queue.push_waited();
 
-		if (/*queue->*/is_full() == false) {
+		if (queue->is_full() == false) {
 			int element = rand() % 100;
-			/*queue->*/enqueue(element);
+			queue->enqueue(element);
 			cout << "Producer " << index << " push " << element << endl;
 		}
 	}
@@ -78,13 +63,13 @@ int main() {
 	cout << "input consumer : ";
 	cin >> c;
 
-//	SYNCHED_QUEUE* que = new SYNCHED_QUEUE();
+	SYNCHED_QUEUE que;
 
-	vector<thread> vec;
+	vector<thread> vec_producer;
 	//thread **list = new thread *[p];
 
 	for (int i = 0; i < p; i++) {
-		vec.push_back(thread(Producer_run,/* que,*/ i));
+		vec_producer.push_back(thread(Producer_run, &que, i));
 		//list[i] = new thread(Producer_run, que, i+1);
 	}
 
@@ -92,11 +77,11 @@ int main() {
 
 
 	for (int i = 0; i < p; i++) {
-		vec[i].join();
+		vec_producer[i].join();
 		//list[i]->join();
 	}
 
-	vec.clear();
+	vec_producer.clear();
 
 /*	for (int i = 0; i < p; i++) {
 			delete(list[i]);
