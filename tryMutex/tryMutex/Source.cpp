7735afa9ea18ab92f3ebb8ec6b624b48c09e7d89
Jeong-hwan Yim <gg7308@bluehole.net>
2014-07-02 15:13:18 +0900

    coding about Producer

--- tryMutex/tryMutex/Source.cpp
+++ tryMutex/tryMutex/Source.cpp
@@ -2,31 +2,75 @@
 #include <thread>
 #include <mutex>
 #include <queue>
+#include <vector>
 
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
-class SYNCHED_QUEUE
+//class SYNCHED_QUEUE
+//{
+//private :
+//	queue<int> que;
+//	mutex mtx;
+//
+//	mutex push_enabled;
+//
+//	const int MAX_COUNT = 10;
+//public :
+//	SYNCHED_QUEUE() {}
+//	void enqueue(int element)
+//	{
+//		mtx.lock();
+//		que.push(element);
+//		mtx.unlock();
+//		//
+//	}
+//	int dequeue()
+//	{
+//		//
+//	}
+//	void push_waited() {
+//		mtx.lock();
+//		push_enabled.lock();
+//		mtx.unlock();
+//	}
+//
+//	bool is_full() {
+//		return que.size() < MAX_COUNT;
+//	}
+//};
+queue<int> que;
+mutex mtx;
+
+mutex push_enabled;
+
+const int MAX_COUNT = 10;
+void enqueue(int element)
 {
-private :
-	queue<int> que;
-	mutex mtx;
+	mtx.lock();
+	que.push(element);
+	mtx.unlock();
+	//
+}
+bool is_full() {
+	return que.size() < MAX_COUNT;
+}
+void Producer_run(/*SYNCHED_QUEUE *queue, */int index) {
+	srand(time(NULL));
 
-	const int MAX_COUNT = 10;
-public :
-	SYNCHED_QUEUE() {}
-	void enqueue(int element)
-	{
-		//
+	while (true) {
+//		queue.push_waited();
+
+		if (/*queue->*/is_full() == false) {
+			int element = rand() % 100;
+			/*queue->*/enqueue(element);
+			cout << "Producer " << index << " push " << element << endl;
+		}
 	}
-	int dequeue()
-	{
-		//
-	}	
-};
-
+}
+	
 int main() {
 	int p, c;
 	cout << "input producer : ";
@@ -34,7 +78,29 @@ int main() {
 	cout << "input consumer : ";
 	cin >> c;
 
-	SYNCHED_QUEUE que;
+//	SYNCHED_QUEUE* que = new SYNCHED_QUEUE();
 
-	
+	vector<thread> vec;
+	//thread **list = new thread *[p];
+
+	for (int i = 0; i < p; i++) {
+		vec.push_back(thread(Producer_run,/* que,*/ i));
+		//list[i] = new thread(Producer_run, que, i+1);
+	}
+
+	// consumer
+
+
+	for (int i = 0; i < p; i++) {
+		vec[i].join();
+		//list[i]->join();
+	}
+
+	vec.clear();
+
+/*	for (int i = 0; i < p; i++) {
+			delete(list[i]);
+	}*/
+//	delete(que);
+//	delete(list);
 }
